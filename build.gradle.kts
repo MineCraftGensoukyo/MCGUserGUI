bc2d3b9267bd621ae2df930c86705ac3a90f82df
Zake-arias <67579820+Zake-arias@users.noreply.github.com>
2021-09-03 02:11:28 +0800

    0.1.0dev01

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+val coroutines_version = "1.4.2"
+
+plugins {
+    java
+    id("io.izzel.taboolib") version "1.26"
+    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+}
+
+taboolib {
+    install("common")
+    install("platform-bukkit")
+    classifier = null
+    version = "6.0.0-53"
+    description {
+        desc("本插件用与为MC幻想乡RPG内容提供图形用户界面的支持")
+        contributors {
+            name("ZakeArias")
+            name("")
+        }
+        dependencies {
+            name("WakeShow").with("bukkit")
+        }
+        load("START")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("ink.ptms.core:v11701:11701:mapped")
+    compileOnly("ink.ptms.core:v11701:11701:universal")
+    compileOnly(kotlin("stdlib"))
+    compileOnly(fileTree("libs"))
+    implementation("org.jetbrains.kotlinx","kotlinx-coroutines-core",coroutines_version)
+    implementation("org.jetbrains.kotlinx","kotlinx-coroutines-jdk8",coroutines_version)
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
---
+++ src/main/kotlin/moe/gensoukyo/gui/MCGUserGUI.kt
@@ -0,0 +1,11 @@
+package moe.gensoukyo.gui
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object MCGUserGUI : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
